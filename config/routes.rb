c1b559d4fce04c34d12fa9d031dda81c155f5b5b
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-01-16 22:13:23 +0200

    feat: aveidos index, root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root "aveidos#index"
   get "/aveidos", to: "aveidos#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
